fb7ddc576472ec6cd34b7adacaa589f59655d0d8
Widitra <widitra.r@gmail.com>
2022-11-03 13:42:47 +0700

    updated

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from turtle import Turtle, Screen
 import random
 
 is_race_on = False
+is_there_a_winner = False
 screen = Screen()
 screen.setup(width=600, height=400)
 turtle_color = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_position = [125, 75, 25, -25, -75, -125]
 all_turtle = []
+all_players_bet = []
 
 for turtle_index in range(0, 6):
     new_turtle = Turtle(shape="turtle")
@@ -15,19 +17,24 @@ for turtle_index in range(0, 6):
     new_turtle.goto(x=-270, y=y_position[turtle_index])
     all_turtle.append(new_turtle)
 
-user_bet = screen.textinput(title="Make Your Bet", prompt="Which turtle will win the race? Enter a color: ")
-if user_bet:
-    is_race_on = True
+num_players = int(screen.textinput(title="Number of Players", prompt="How many players are playing? "))
+for player in range(0, num_players):
+    current_bet = screen.textinput(title="Player's bet", prompt=f"Input color bet for player {player+1}")
+    all_players_bet.append(current_bet)
+
+is_race_on = True
 
 while is_race_on:
     for turtle in all_turtle:
         if turtle.xcor() > 260:
             is_race_on = False
             winning_turtle = turtle.pencolor()
-            if winning_turtle == user_bet:
-                print(f"You've won the bet! The {winning_turtle} turtle is the winner!")
-            else:
-                print(f"You've lost the bet! The {winning_turtle} turtle is the winner!")
+            for player in range(0, len(all_players_bet)):
+                if all_players_bet[player] == winning_turtle:
+                    print(f"the {winning_turtle} turtle got 1st place! The winner is player {player+1}!")
+                    is_there_a_winner = True
+            if not is_there_a_winner:
+                print(f"the {winning_turtle} turtle got 1st place! None of the players won the match!")
 
         random_distance = random.randint(0, 10)
         turtle.forward(random_distance)
